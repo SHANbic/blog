876181998c3f7278c777c7651220bf905b3b2eac
Pierre Lambert <supagoof924@hotmail.com>
2019-05-16 17:23:23 +0200

    [ REFACTO ] Refactored UserHeader component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,14 +7,14 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props;
     if (!user) return null;
     else return <div className="header">{user.name}</div>;
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(
